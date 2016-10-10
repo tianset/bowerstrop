5395d1bb363e461c1ad7c7f4c65c896cdcc1025c
tutiantian <1072573153@qq.com>
2016-10-10 09:41:04 +0800

    index

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('bowerstrop')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('bowerstrop', ['ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
